859321611983f06d87db7f16370b621f07743cb1
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-01-25 12:13:29 +0100

    Populating initial data in database

--- app/src/main/java/com/aloine/notepadmvvm/database/NoteDatabase.java
+++ app/src/main/java/com/aloine/notepadmvvm/database/NoteDatabase.java
@@ -2,13 +2,16 @@ package com.aloine.notepadmvvm.database;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import com.aloine.notepadmvvm.contract.NoteDao;
 import com.aloine.notepadmvvm.model.Note;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Note.class, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -22,8 +25,30 @@ public abstract class NoteDatabase extends RoomDatabase {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),NoteDatabase.class,"note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+        public PopulateDbAsyncTask(NoteDatabase db) {
+        noteDao = db.noteDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+            return null;
+        }
     }
 }
