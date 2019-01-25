83e5537b1b5fce6f208b5504daf0c43b8c1da451
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-01-25 16:37:54 +0100

    Add note working

---
+++ app/src/main/java/com/aloine/notepadmvvm/AddEditNoteActivity.java
@@ -0,0 +1,75 @@
+package com.aloine.notepadmvvm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class AddEditNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_TITLE = "com.aloine.notepadmvvm.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION = "com.aloine.notepadmvvm.EXTRA_DESSCRIPTION";
+    public static final String EXTRA_PRIORITY = "com.aloine.notepadmvvm.EXTRA-PRIORITY";
+
+    private EditText mEditTextTitle,mEditTextDescription;
+    private NumberPicker mNumberPicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_note);
+        init();
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_cancel);
+        setTitle("Add Note");
+    }
+
+    private void init() {
+        mEditTextTitle = findViewById(R.id.edit_text_title);
+        mEditTextDescription = findViewById(R.id.edit_text_description);
+        mNumberPicker = findViewById(R.id.number_picker_priority);
+        mNumberPicker.setMinValue(1);
+        mNumberPicker.setMaxValue(10);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         new MenuInflater(this).inflate(R.menu.add_note_menu,menu);
+         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);         
+
+        }
+       
+    }
+
+    private void saveNote() {
+        String title = mEditTextTitle.getText().toString();
+        String description = mEditTextDescription.getText().toString();
+        int priority = mNumberPicker.getValue();
+
+        if (title.trim().isEmpty() || description.trim().isEmpty()) {
+            Toast.makeText(this, "Please insert values for those parameters", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE,title);
+        data.putExtra(EXTRA_DESCRIPTION,description);
+        data.putExtra(EXTRA_PRIORITY,priority);
+
+        setResult(RESULT_OK,data);
+        finish();
+
+    }
+}
--- app/src/main/java/com/aloine/notepadmvvm/MainActivity.java
+++ app/src/main/java/com/aloine/notepadmvvm/MainActivity.java
@@ -2,11 +2,15 @@ package com.aloine.notepadmvvm;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.aloine.notepadmvvm.adapter.NoteAdapter;
 import com.aloine.notepadmvvm.database.model.Note;
@@ -15,13 +19,23 @@ import com.aloine.notepadmvvm.database.viewmodel.NoteViewModel;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ADD_NOTE_REQUEST = 1;
 private NoteViewModel noteViewModel;
 private NoteAdapter noteAdapter;
+private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        fab = findViewById(R.id.button_add_note);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AddEditNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST );
+            }
+        });
 
     noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
     noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
@@ -40,5 +54,23 @@ private NoteAdapter noteAdapter;
         recyclerView.setHasFixedSize(true);
         noteAdapter = new NoteAdapter();
         recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddEditNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddEditNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddEditNoteActivity.EXTRA_PRIORITY,1);
+
+            Note note = new Note(title, description, priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this, "Successfully inserted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
